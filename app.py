f72c913ca06db390d034db539563ba44665420e5
jgillas <125215083+jgillas@users.noreply.github.com>
2023-06-15 21:38:58 -0500

    update

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+
+# Replace the database_url with your actual database URL
+engine_sqlite = create_engine('sqlite:///teen_births.sqlite')
+
+path = os.path.join('Data','NCHS_-_U.S._and_State_Trends_on_Teen_Births.csv')
+
+df = pd.read_csv(path)
+
+df.to_sql('birth_rates', engine_sqlite, if_exists='replace', index=False)
